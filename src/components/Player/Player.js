75e1a8519f98b09544f2fa6f310d17205c6cc621
AndrRomanenko <andy_snews@mail.ru>
2019-06-07 12:54:19 +0300

    player ui & buttons

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,16 +1,44 @@
 import React from "react";
 import SoundCloudPlayer from 'react-player/lib/players/SoundCloud';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faFastBackward } from '@fortawesome/free-solid-svg-icons';
+import { faFastForward } from '@fortawesome/free-solid-svg-icons';
+
+import './Player.css';
+
 
 const Player = (props) => {
   return (
     <div className="player-container">
       <SoundCloudPlayer
+        width="100%"
         className='react-player'
-        url='https://soundcloud.com/tsimashei/malyy-povzroslel-fetre-prod'
+        url="https://soundcloud.com/tsimashei/s3q1t3aejgsz"
       />
       <div className="player-buttons">
-        БУТТОНС!
+        <div className="control-btn">
+          <FontAwesomeIcon
+            icon={faFastBackward}
+            color="white"
+            size="2x"
+          />
+        </div>
+        <div className="control-btn">
+          <FontAwesomeIcon
+            icon={faPlay}
+            color="white"
+            size="2x"
+          />
+        </div>
+        <div className="control-btn">
+          <FontAwesomeIcon
+            icon={faFastForward}
+            color="white"
+            size="2x"
+          />
+        </div>
       </div>
     </div>
   );
